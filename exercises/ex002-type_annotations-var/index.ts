90afebd734a64c51cb1010005ab5a7023b85766e
claudiobentodaladev <programmer.claudio@gmail.com>
2026-03-11 08:24:52 +0100

    TYPE ANNOTATIONS: with var

---
+++ exercises/ex002-type_annotations-var/index.ts
@@ -0,0 +1,10 @@
+//  Type annotacions
+
+const myName: string = "claudio";
+const myAge: number = 16;
+const isAdult: boolean = myAge >= 18;
+
+console.log(`Hi, i'm ${myName} and i'm ${myAge} years old`);
+
+if (isAdult) console.log(`I'm adult`);
+else console.log(`I'm not adult`);
